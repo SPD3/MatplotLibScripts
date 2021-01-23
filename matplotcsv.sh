7c462133a732711e82e94a151a37587525e112f7
SPD3 <sean@ayerdoyles.org>
2021-01-23 09:29:29 -0500

    Changed bash file to use the pwd instead of a set path

--- matplotcsv.sh
+++ matplotcsv.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-/Library/Frameworks/Python.framework/Versions/3.8/bin/python3 /Users/seandoyle/MatplotlibScripts/MatplotLibScripts/plot.py $1
+/Library/Frameworks/Python.framework/Versions/3.8/bin/python3 $PWD/MatplotLibScripts/plot.py $1
